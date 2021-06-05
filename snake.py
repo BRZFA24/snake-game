f8d05ef0364941dbd500a9c8a43c8af69484e621
Luis E. Rivera Velazquez <79478746+BRZFA24@users.noreply.github.com>
2021-06-04 20:22:55 -0400

    Add files via upload
    A simple GUI of the popular game of snake.

---
+++ snake.py
@@ -0,0 +1,61 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.snakes = []
+        self.create_snake()
+        self.head = self.snakes[0]
+        self.tail = self.snakes[2]
+
+    def create_snake(self):
+        for snake_position in STARTING_POSITIONS:
+            self.add_snake(snake_position)
+
+    def add_snake(self, position):
+        new_snake = Turtle("square")
+        new_snake.penup()
+        new_snake.goto(position)
+        new_snake.color("white")
+        self.snakes.append(new_snake)
+
+    def reset(self):
+        for seg in self.snakes:
+            seg.goto(1000, 1000)
+        self.snakes.clear()
+        self.create_snake()
+        self.head = self.snakes[0]
+
+    def extend(self):
+        #Add a new segment for the snake.
+        self.add_snake(self.snakes[-1].position())
+
+    def move(self):
+        for snake_num in range(len(self.snakes) - 1, 0, -1):
+            new_x = self.snakes[snake_num - 1].xcor()
+            new_y = self.snakes[snake_num - 1].ycor()
+            self.snakes[snake_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
